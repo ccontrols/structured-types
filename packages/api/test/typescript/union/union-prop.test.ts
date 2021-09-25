3e4540d8655f6b26646c3ebab4647c3095fc9cc8
atanasster <atanasster@gmail.com>
2021-09-25 05:52:39 -0400

    test: update union test

--- packages/api/test/typescript/union/union-prop.test.ts
+++ packages/api/test/typescript/union/union-prop.test.ts
@@ -2,6 +2,38 @@ import path from 'path';
 import { parseFiles } from '../../../src/index';
 
 describe('union', () => {
+  it('members-comments', () => {
+    const results = parseFiles([
+      path.resolve(__dirname, 'members-comments.ts'),
+    ]);
+    expect(results).toEqual({
+      TextAlignment: {
+        name: 'TextAlignment',
+        kind: 4,
+        properties: [
+          {
+            kind: 1,
+            type: 'Left',
+            value: 'left',
+            description: 'Left-aligns the text.',
+          },
+          {
+            kind: 1,
+            type: 'Right',
+            value: 'right',
+            description: 'Right-aligns the text.',
+          },
+          {
+            kind: 1,
+            type: 'Center',
+            value: 'center',
+            description: 'Centers the text horizontally.',
+          },
+        ],
+        description: 'Controls the alignment of text when printed.',
+      },
+    });
+  });
   it('export const', () => {
     const results = parseFiles([path.resolve(__dirname, 'export-const.ts')]);
     expect(results).toEqual({
