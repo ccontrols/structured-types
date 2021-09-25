76f3c2b2b97d09a2e230d3922112eaaf0049b673
atanasster <atanasster@gmail.com>
2021-09-25 00:26:39 -0400

    fix(site): force js extension

--- site/pages/api/documentation.ts
+++ site/pages/api/documentation.ts
@@ -8,19 +8,14 @@ export default async (
   req: NextApiRequest,
   res: NextApiResponse,
 ): Promise<void> => {
-  const { code, tsoptions } = req.query as {
+  const { code } = req.query as {
     code?: string;
     tsoptions?: string;
   };
-  const options = {
-    ...(tsoptions ? JSON.parse(tsoptions) : undefined),
-  };
-  const { lang = 'typescript' } = options || {};
-  const extension = lang === 'javascript' ? 'js' : 'ts';
   const host = ts.createCompilerHost({});
   if (host.createHash) {
     const name = host.createHash(Math.random().toString()).substring(0, 12);
-    const fileName = `${tmpdir}${sep}${name}.${extension}`;
+    const fileName = `${tmpdir}${sep}${name}.js`;
 
     ts.sys.writeFile(fileName, code || '');
     documentation
