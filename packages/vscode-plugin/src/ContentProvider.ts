eb81a02cdba9cde493e4e054135ba40257db6e30
atanasster <atanasster@gmail.com>
2021-10-22 23:45:02 -0400

    chore(vscode-extension): split utils

--- packages/vscode-plugin/src/ContentProvider.ts
+++ packages/vscode-plugin/src/ContentProvider.ts
@@ -2,6 +2,7 @@ import * as path from 'path';
 import * as vscode from 'vscode';
 import { extractProps } from './extract-props';
 import { nodesToHTML } from './nodeToHTML';
+import { getUri } from './utils';
 
 type PanelStore = {
   panel?: vscode.WebviewPanel;
@@ -137,62 +138,4 @@ export class ContentProvider {
 			</body>
 			</html>`;
   }
-
-  openSymbol(filePath: string, symbolName: string): void {
-    vscode.workspace
-      .openTextDocument(vscode.Uri.file(filePath))
-      .then((document) => {
-        vscode.window.showTextDocument(document).then(() => {
-          goToSymbol(document, symbolName);
-        });
-      });
-  }
-}
-
-async function getSymbols(
-  document: vscode.TextDocument,
-): Promise<vscode.DocumentSymbol[]> {
-  return new Promise(async (resolve) => {
-    let symbols = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>(
-      'vscode.executeDocumentSymbolProvider',
-      document.uri,
-    );
-    // tslint:disable-line
-    if (!symbols || symbols.length === 0) {
-      setTimeout(async () => {
-        symbols = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>(
-          'vscode.executeDocumentSymbolProvider',
-          document.uri,
-        );
-        // tslint:disable-line
-        return resolve(symbols || []);
-      }, 1200);
-    } else {
-      return resolve(symbols || []);
-    }
-  });
-}
-
-async function goToSymbol(document: vscode.TextDocument, symbolName: string) {
-  const symbols = await getSymbols(document);
-  const findSymbol = symbols.find((symbol) => symbol.name === symbolName);
-  const activeTextEditor = vscode.window.activeTextEditor;
-  if (findSymbol && activeTextEditor) {
-    activeTextEditor.selection = new vscode.Selection(
-      findSymbol.range.start,
-      findSymbol.range.start,
-    );
-    activeTextEditor.revealRange(
-      findSymbol.range,
-      vscode.TextEditorRevealType.AtTop,
-    );
-  }
-}
-
-function getUri(
-  webview: vscode.Webview,
-  extensionUri: vscode.Uri,
-  pathList: string[],
-): vscode.Uri {
-  return webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, ...pathList));
 }
---
+++ packages/vscode-plugin/src/utils.ts
@@ -0,0 +1,85 @@
+import * as vscode from 'vscode';
+
+import { PropType } from '@structured-types/api';
+
+export async function getSymbols(
+  document: vscode.TextDocument,
+): Promise<vscode.DocumentSymbol[]> {
+  return new Promise(async (resolve) => {
+    let symbols = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>(
+      'vscode.executeDocumentSymbolProvider',
+      document.uri,
+    );
+    // tslint:disable-line
+    if (!symbols || symbols.length === 0) {
+      setTimeout(async () => {
+        symbols = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>(
+          'vscode.executeDocumentSymbolProvider',
+          document.uri,
+        );
+        // tslint:disable-line
+        return resolve(symbols || []);
+      }, 1200);
+    } else {
+      return resolve(symbols || []);
+    }
+  });
+}
+
+async function goToSymbol(
+  document: vscode.TextDocument,
+  symbolName: string,
+): Promise<void> {
+  const symbols = await getSymbols(document);
+  const findSymbol = symbols.find((symbol) => symbol.name === symbolName);
+  const activeTextEditor = vscode.window.activeTextEditor;
+  if (findSymbol && activeTextEditor) {
+    activeTextEditor.selection = new vscode.Selection(
+      findSymbol.range.start,
+      findSymbol.range.start,
+    );
+    activeTextEditor.revealRange(
+      findSymbol.range,
+      vscode.TextEditorRevealType.AtTop,
+    );
+  }
+}
+
+export const openSymbol = (filePath: string, symbolName: string): void => {
+  vscode.workspace
+    .openTextDocument(vscode.Uri.file(filePath))
+    .then((document) => {
+      vscode.window.showTextDocument(document).then(() => {
+        goToSymbol(document, symbolName);
+      });
+    });
+};
+
+const goToLocation = (loc: PropType['loc']): void => {
+  const activeTextEditor = vscode.window.activeTextEditor;
+  if (activeTextEditor) {
+    const range = new vscode.Range(
+      new vscode.Position(loc.line, loc.col),
+      new vscode.Position(loc.line, loc.col),
+    );
+    activeTextEditor.selection = new vscode.Selection(range.start, range.start);
+    activeTextEditor.revealRange(range, vscode.TextEditorRevealType.AtTop);
+  }
+};
+
+export const openLocation = (filePath: string, loc: PropType['loc']): void => {
+  vscode.workspace
+    .openTextDocument(vscode.Uri.file(filePath))
+    .then((document) => {
+      vscode.window.showTextDocument(document).then(() => {
+        goToLocation(loc);
+      });
+    });
+};
+export function getUri(
+  webview: vscode.Webview,
+  extensionUri: vscode.Uri,
+  pathList: string[],
+): vscode.Uri {
+  return webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, ...pathList));
+}
