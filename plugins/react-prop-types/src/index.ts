d7a6d33d3f17b280cf0add1b0b2a0cdf0914b51c
atanasster <atanasster@gmail.com>
2021-11-09 01:47:18 -0500

    fix(prop-types-plugin): collectInheritance false

--- plugins/react-prop-types/src/index.ts
+++ plugins/react-prop-types/src/index.ts
@@ -244,6 +244,7 @@ export default {
           collectGenerics: false,
           collectParameters: false,
           collectProperties: false,
+          collectInheritance: false,
         };
       }
     }
