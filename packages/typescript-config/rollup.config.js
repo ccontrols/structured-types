67f30b6ddf44e4656dde2171f520cb3392b2ad74
atanasster <atanasster@gmail.com>
2021-12-16 11:51:45 +0200

    fix(ts-config): rollup resolve

--- packages/typescript-config/rollup.config.js
+++ packages/typescript-config/rollup.config.js
@@ -1,6 +1,8 @@
+import resolve from '@rollup/plugin-node-resolve';
 import { config } from '../../rollup-config';
 
 export default config({
   external: ['typescript'],
   input: ['./src/index.ts'],
+  plugins: [resolve({ browser: true })],
 });
