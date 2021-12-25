835a18bf28e8aae01f75b90e58a956d0fad7112b
atanasster <atanasster@gmail.com>
2021-12-25 01:20:13 -0500

    fix(api-docs): description lines to space

--- packages/api-docs/src/sections/description.ts
+++ packages/api-docs/src/sections/description.ts
@@ -1,18 +1,11 @@
 import { PropType } from '@structured-types/api';
-import { DocumentationNode, NodeKind } from '../types';
+import { DocumentationNode } from '../types';
 import { paragraphNode } from '../blocks/paragraph';
 
 export const descriptionSection = (
   prop: PropType,
 ): DocumentationNode[] | undefined => {
   return prop.description
-    ? [
-        paragraphNode(
-          prop.description.split('\n').map((d) => ({
-            kind: NodeKind.Text,
-            value: d,
-          })),
-        ),
-      ]
+    ? [paragraphNode(prop.description.split('\n').join(' '))]
     : undefined;
 };
