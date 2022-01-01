b940b0f102864767c84352b93dda658842277e93
atanasster <atanasster@gmail.com>
2021-12-31 23:28:07 -0500

    fix: ts type any

--- packages/instant-documentation-plugin/src/vendor/tsWorker.d.ts
+++ packages/instant-documentation-plugin/src/vendor/tsWorker.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import ts from 'typescript';
 export declare class TypeScriptWorker implements ts.LanguageServiceHost {
   private _ctx;
