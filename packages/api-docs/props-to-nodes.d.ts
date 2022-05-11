a02a3ada9652f074344eb81db53769dd07338e5c
atanasster <atanasster@gmail.com>
2022-05-11 16:57:23 +0300

    fix(api-docs): export props-to-nodes

---
+++ packages/api-docs/props-to-nodes.d.ts
@@ -0,0 +1,1 @@
+export * from './dist/props-to-nodes.d';
--- packages/api-docs/rollup.config.js
+++ packages/api-docs/rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import { config } from '../../rollup-config';
 
 export default config({
-  input: ['./src/index.ts', './src/types.ts'],
+  input: ['./src/index.ts', './src/types.ts', './src/props-to-nodes.ts'],
   external: ['typescript'],
   plugins: [resolve({ browser: true, preferBuiltins: false })],
 });
